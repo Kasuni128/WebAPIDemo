45f2b856894c85ad6d795fb1df15ea9e58c065ac
Kasuni128 <kasuniwaththage128@gmail.com>
2026-04-13 23:07:05 +0530

    Created Database Context

---
+++ WebAPIDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using WebAPIDemo.Model;
+
+namespace WebAPIDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Shirt> Shirts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //data seeding
+            modelBuilder.Entity<Shirt>().HasData(
+                 new Shirt { ShirtId = 1, Brand = "Red Shirt", Color = "Red", Size = 8, Gender = "Men", Price = 29.99 },
+                 new Shirt { ShirtId = 2, Brand = "Blue Shirt", Color = "Blue", Size = 6, Gender = "Women", Price = 19.99 },
+                 new Shirt { ShirtId = 3, Brand = "Green Shirt", Color = "Green", Size = 12, Gender = "Men", Price = 39.99 }
+            );
+        }       
+    }
+}
