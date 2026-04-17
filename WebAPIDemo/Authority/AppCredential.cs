b73a546150f947f97a749a387988a6551f57c6a5
Kasuni128 <kasuniwaththage128@gmail.com>
2026-04-17 22:47:56 +0530

    Implement API client authentication endpoint with client credentials

---
+++ WebAPIDemo/Authority/AppCredential.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPIDemo.Authority
+{
+    public class AppCredential
+    {
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+    }
+}
---
+++ WebAPIDemo/Controller/AuthorityController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIDemo.Authority;
+
+namespace WebAPIDemo.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorityController : ControllerBase
+    {
+        [HttpPost("auth")]
+        public IActionResult Authenticate([FromBody] AppCredential credential)
+        {
+            if (AppRepository.Authenticate(credential.ClientId, credential.ClientSecret))
+            {
+
+                return Ok(new
+                {
+                    access_toke = CreateToken(credential.ClientId),
+                    expires_at = DateTime.UtcNow.AddMinutes(10)
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Unauthorized", "You are not authorized.");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status401Unauthorized
+                };
+                return new UnauthorizedObjectResult(problemDetails);
+            }
+        }
+
+        private string CreateToken(string clientId)
+        {
+            return string.Empty;
+        }
+    }
+}
